4a0c67bc5dc1a00b031f08c0b43b0f9b3cc98bd0
John Hume <duelin.markers@gmail.com>
2017-07-23 15:08:31 -0500

    Add POST / endpoint w/ JSON (de)serialization of a Todo
    This passes the second test.
    
    "the api root responds to a POST with the todo which was posted to it"

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,29 @@
 
 extern crate rocket;
 extern crate rocket_cors;
+extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+use rocket_contrib::Json;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Serialize, Deserialize)]
+struct Todo {
+    title: String,
+}
+
+#[post("/", data = "<todo>")]
+fn create_todo(todo: Json<Todo>) -> Json<Todo> {
+    todo
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", routes![index, create_todo])
         .attach(rocket_cors::Cors::default())
         .launch();
 }
