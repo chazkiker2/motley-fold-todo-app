3f6e1adfb4cdfe1e30dd2ee0bba3addd4b0f8db9
John Hume <duelin.markers@gmail.com>
2017-07-24 20:10:52 -0500

    Introduce state. Add, clear, and list it.
    So much for tiny changes! This passes the first of the "storing
    new todos by posting to the root url" tests:
    "adds a new todo to the list of todos at the root url"
    
    Several questionable decisions here.

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,49 @@ extern crate rocket_cors;
 extern crate rocket_contrib;
 #[macro_use] extern crate serde_derive;
 
+use rocket::State;
+use rocket::http::Status;
+use rocket::response::Failure;
 use rocket_contrib::Json;
+use std::sync::Mutex;
 
 #[get("/")]
-fn index() -> Json<Vec<Todo>> {
-    Json(vec![])
+fn index(state: State<Mutex<Vec<Todo>>>) -> Result<Json<Vec<Todo>>, Failure> {
+    state.lock()
+        .map_err(|_| Failure(Status::InternalServerError))
+        .map(|todos| Json(todos.clone()))
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 struct Todo {
     title: String,
 }
 
-#[post("/", data = "<todo>")]
-fn create_todo(todo: Json<Todo>) -> Json<Todo> {
-    todo
+#[post("/", data = "<todo_json>")]
+fn create_todo(todo_json: Json<Todo>, state: State<Mutex<Vec<Todo>>>) -> Result<Json<Todo>, Failure> {
+    state.lock()
+        .map_err(|_| Failure(Status::InternalServerError))
+        .map(|mut todos| {
+            let todo = todo_json.into_inner();
+            todos.push(todo.clone());
+            Json(todo)
+        })
 }
 
 #[delete("/")]
-fn delete_all() -> Json<Vec<Todo>> {
-    index()
+fn delete_all(state: State<Mutex<Vec<Todo>>>) -> Result<Json<Vec<Todo>>, Failure> {
+    state.lock()
+        .map_err(|_| Failure(Status::InternalServerError))
+        .map(|mut todos| {
+            todos.clear();
+            Json(todos.clone())
+        })
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![index, create_todo, delete_all])
         .attach(rocket_cors::Cors::default())
+        .manage(Mutex::new(Vec::<Todo>::new()))
         .launch();
 }
